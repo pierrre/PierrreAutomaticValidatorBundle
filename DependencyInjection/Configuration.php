c5af529ca8d0cbe1cb4e9ec2bc0ed198ce17b90b
Pierre Durand <pierre@plyce.com>
2012-07-10 23:21:26 +0200

    Fix configuration system

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pierrre\AutomaticValidatorBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface {
+	/**
+	 * @see Symfony\Component\Config\Definition.ConfigurationInterface::getConfigTreeBuilder()
+	 */
+	public function getConfigTreeBuilder(){
+		$treeBuilder = new TreeBuilder();
+		
+		$treeBuilder->root('pierrre_automatic_validator')
+		;
+		
+		return $treeBuilder;
+	}
+}
--- DependencyInjection/PierrreAutomaticValidatorExtension.php
+++ DependencyInjection/PierrreAutomaticValidatorExtension.php
@@ -2,34 +2,20 @@
 
 namespace Pierrre\AutomaticValidatorBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class PierrreAutomaticValidatorExtension extends Extension implements ConfigurationInterface{
+class PierrreAutomaticValidatorExtension extends Extension {
 	/**
-	 * @see Symfony\Component\DependencyInjection\Extension.ExtensionInterface::load()
+	 * @see Symfony\Component\DependencyInjection\Extension\ExtensionInterface::load()
 	 */
 	public function load(array $configs, ContainerBuilder $container){
 		$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 		$loader->load('services.yml');
 		
-		$config = $this->processConfiguration($this, $configs);
+		$config = $this->processConfiguration(new Configuration(), $configs);
 		$alias = $this->getAlias();
-	}
-	
-	/**
-	 * @see Symfony\Component\Config\Definition.ConfigurationInterface::getConfigTreeBuilder()
-	 */
-	public function getConfigTreeBuilder(){
-		$treeBuilder = new TreeBuilder();
-		
-		$treeBuilder->root($this->getAlias())
-		;
-		
-		return $treeBuilder;
 	}
 }
